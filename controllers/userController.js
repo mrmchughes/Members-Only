20bbce2a3efacafa42c7f0f724472dc17e50bfa9
Michael <mrmchughes27@gmail.com>
2022-07-21 13:13:41 -0700

    password validation added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 let User = require("../models/user");
 
 // Display user create form on GET.
@@ -6,16 +7,38 @@ exports.create_user_get = function (req, res, next) {
 };
 
 // Handle user create on POST.
-exports.create_user_post = function (req, res, next) {
-  const user = new User({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    username: req.body.username,
-    password: req.body.password,
-  }).save((err) => {
-    if (err) {
-      return next(err);
+exports.create_user_post = [
+  // Validate and sanitize fields.
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username must be specified.")
+    .isAlphanumeric()
+    .withMessage("Username has non-alphanumeric characters."),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .escape()
+    .withMessage("Password must be at least 8 characters"),
+  body("confirm-password").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Password confirmation does not match password");
     }
-    res.redirect("/");
-  });
-};
+    return true;
+  }),
+
+  // Process request after validation and sanitization.
+  (req, res) => {
+    // Create an User object with escaped and trimmed data.
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  },
+];
