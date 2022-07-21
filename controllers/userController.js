3f53a5583b413367cb0410ac34f2c2528ceda1a3
Michael <mrmchughes27@gmail.com>
2022-07-21 14:57:56 -0700

    password confirmation works, removed alphanumeric validator for username, username can now include numbers

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,32 +13,38 @@ exports.create_user_post = [
     .trim()
     .isLength({ min: 1 })
     .escape()
-    .withMessage("Username must be specified.")
-    .isAlphanumeric()
-    .withMessage("Username has non-alphanumeric characters."),
+    .withMessage("Username must be specified."),
   body("password")
     .trim()
     .isLength({ min: 8 })
     .escape()
     .withMessage("Password must be at least 8 characters"),
-  body("confirm-password").custom((value, { req }) => {
+  body("confirmpassword").custom((value, { req }) => {
+    console.log(value);
+
     if (value !== req.body.password) {
       throw new Error("Password confirmation does not match password");
     }
+
+    // Indicates the success of this synchronous custom validator
     return true;
   }),
-
   // Process request after validation and sanitization.
-  (req, res) => {
-    // Create an User object with escaped and trimmed data.
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    }).save((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).redirect("/sign-up");
+    } else {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+      }).save((err) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect("/");
+      });
+    }
   },
 ];
